7eebcecdbe7da833089876b9ffcda09adfb1ded1
JaHyeon_Gu <jahyungu@naver.com>
2022-02-13 19:13:16 +0900

    solve 2 questions

--- Number_Theory/9375.py
+++ Number_Theory/9375.py
@@ -1,11 +1,24 @@
-from math import factorial
 import sys
-input = sys.stdin.readline()
+from itertools import product
 
-n = int(input)
-c = int(input)
-clothes = []
+n = int(sys.stdin.readline())
+
 for _ in range(n):
-    for _ in range(c):
-        a, b = input.split()
-        clothes.append(b)
+    k = int(sys.stdin.readline())
+    answer = 1
+    dict = {}
+    for i in range(k):
+        type = sys.stdin.readline().split()[1]
+
+        # 옷 종류 별 갯수 사전에 추가
+        if type in dict.keys():
+            dict[type] += 1
+        else:
+            dict[type] = 1
+
+    # 안 입는 경우 (1) 더해서 종류별 경우의 수 구하기
+    for j in dict.values():
+        answer *= (j+1)
+
+    # 다 벗는 경우 제외
+    print(answer-1)
