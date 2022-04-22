40d76066c61b530d0685067ca9f60dfb676aa837
JaHyeon_Gu <jahyungu@naver.com>
2022-04-22 11:59:00 +0900

    solve 1 Brute_Force

---
+++ Brute_Force/1018.py
@@ -0,0 +1,41 @@
+v, h = map(int, input().split())
+INF = int(10e9)
+ans = INF
+
+case_1 = [
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
+]
+
+case_2 = [
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
+]
+matrix = [list(input()) for _ in range(v)]
+
+for i in range(v-7):    # [0, 1, 2]
+    for j in range(h - 7):  # [0, 1, 2, 3, 4, 5]
+        ans_1 = 0
+        ans_2 = 0
+        for x in range(i, i+8):
+            for y in range(j, j+8):
+                if matrix[x][y] != case_1[x - i][y - j]:
+                    ans_1 += 1
+                elif matrix[x][y] != case_2[x - i][y - j]:
+                    ans_2 += 1
+
+        ans = min(ans, min(ans_1, ans_2))
+
+print(ans)
