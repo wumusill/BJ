3d943ad636d22516516a65c45e22e20ee2edbd39
JaHyeon_Gu <jahyungu@naver.com>
2023-04-15 18:57:00 +0900

    DP another solution

--- Dynamic_Programming/11055.py
+++ Dynamic_Programming/11055.py
@@ -13,3 +13,24 @@ for i in range(n):
             dp[j] = max(dp[j], dp[i] + l[j])
 
 print(max(dp))
+
+####################################################################################
+
+n = int(input())
+
+sequence = list(map(int, input().split()))
+
+dp_table = [0] * 1001
+
+for j in range(n):
+    num = sequence[j]
+    # True == 1
+    if dp_table[num]:
+        dp_table[num] = max(max(dp_table[:num]) + num, dp_table[num])
+    # False == 0
+    # dp_table[num] == 0
+    # 값을 인덱스로 하여 해당 인덱스에 나올 수 있는 가장 큰 수 저장
+    else:
+        dp_table[num] = max(dp_table[:num]) + num
+
+print(max(dp_table))
