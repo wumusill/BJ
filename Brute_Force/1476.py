d3f54f4b56a0417ca0f0c50e4ffdd330c210f7d4
JaHyeon_Gu <jahyungu@naver.com>
2023-07-20 15:52:55 +0900

    solve 1 Brute Force

--- Brute_Force/1476.py
+++ Brute_Force/1476.py
@@ -16,3 +16,13 @@ while True:
         s = 1
     if m == 20:
         m = 1
+#############################################################
+# 더 간단한 다른 풀이 방법
+E, S, M = map(int, input().strip().split())
+ans = 1
+
+while True:
+    if (ans - E) % 15 == 0 and (ans - S) % 28 == 0 and (ans - M) % 19 == 0:
+        print(ans)
+        break
+    ans += 1
