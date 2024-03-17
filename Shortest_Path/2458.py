df5fa46291dd03672be867908477e44e67986418
JaHyeon_Gu <jahyungu@naver.com>
2024-03-18 00:23:46 +0900

    solve 1 Floyd-Warshall & dfs

--- Shortest_Path/2458.py
+++ Shortest_Path/2458.py
@@ -1,4 +1,5 @@
 # 본인보다 키가 작거나 큰 인원이 (n - 1)명이라면 자신의 키 순서를 알 수 있음
+# pypy3 : 113,300 KB, 1,344ms
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
@@ -21,5 +22,44 @@ for a in range(1, n + 1):
         know += height[a][b] + height[b][a]             # 본인보다 키가 작거나 키가 큰 인원 합계 계산
     if know == n - 1:                                   # 합계가 본인을 제외한 n - 1 이라면, 키 순서를 알 수 있음
         answer += 1                                     # 키 순서 알 수 있는 인원 갱신
+
+print(answer)
+#######################################################################################################################
+# python3 : 35,380 KB, 872ms
+import sys
+
+
+def dfs(graph, node):                               # dfs를 수행하여 자식 노드의 개수 반환
+    visited = [False] * (n + 1)
+    stack = [node]
+    result = 0
+
+    while stack:
+        now = stack.pop()
+        for next in graph[now]:
+            if not visited[next]:
+                stack.append(next)
+                visited[next] = True
+                result += 1
+
+    return result
+
+
+n, m = map(int, sys.stdin.readline().split())
+asc = [[] for _ in range(n + 1)]                    # 키 오름차순 그래프 a가 b보다 작음 -> graph[a].append(b)
+desc = [[] for _ in range(n + 1)]                   # 키 내림차순 그래프 a가 b보다 작음 -> graph[b].append(a)
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    asc[a].append(b)
+    desc[b].append(a)
+
+answer = 0
+for start in range(1, n + 1):
+    res_asc = dfs(asc, start)                       # 키가 start보다 큰 사람의 수 계산
+    res_desc = dfs(desc, start)                     # 키가 start보다 작은 사람의 수 계산
+
+    if res_asc + res_desc == n - 1:                 # 계산된 두 값의 합이 자기 자신을 제외한 (n - 1)과 같다면 본인의 키 순서 파악 가능
+        answer += 1
 
 print(answer)
